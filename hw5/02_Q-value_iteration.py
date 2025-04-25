1d016acadacea25b1c46eed8f96eb6afecee9c1d
Abraham <asoteloa@outlook.com>
2025-04-25 13:40:07 -0600

    Homework 5 - Problem 2. Q-Value Iteration - Reward function optimized

--- hw5/02_Q-value_iteration.py
+++ hw5/02_Q-value_iteration.py
@@ -15,24 +15,36 @@ T = np.array([                                         # States
 ])
 # Actions     M                          C
 
-# Filling R
-R = np.zeros(T.shape)
-for s in range(N_STATES):
-  for a in range(N_ACTIONS):
-    for sp in range(N_STATES):
-      if s != 0:
-        if s == sp:
-          R[s,a,sp] = (s + 4)**(-1/2)
-        else:
-          R[s,a,sp] = np.abs(sp - s)**(1/3)
+''' Reward function R(s, a, s'):
+If s == 0:
+  - Rewards are 0 (no rewards are defined for state 0).
+If s ≠ 0:
+  - If s == s' (stay in the same state):
+    - Reward is (s + 4)^(-1/2)
+  - If s ≠ s' (transition to a different state):
+    - Reward is |s' - s|^(1/3) '''
+# Adding an extra dimension to compare s and s' combinations
+S  = np.arange(N_STATES).reshape(-1, 1, 1)  # State
+Sn = np.arange(N_STATES).reshape(1, 1, -1)  # Next state
+# Create two masks to control the values or R, embedding s ≠ 0 on them
+mask_same = (S != 0) & (S == Sn)                   # (6,1,6)
+mask_diff = (S != 0) & (S != Sn)
+# Easier to get the reward by multiplying with the masks than masking on R
+reward_same = mask_same * (S + 4)**(-1/2)          # (6,1,6)
+reward_diff = mask_diff * np.abs(Sn - S)**(1/3)
+# Masks are mutually exclusive, so I can add them
+rewards = reward_same + reward_diff                # (6,1,6)
+# Numpy can broadcast to (6,2,1) by adding the rewards to 0(6,2,6)
+R = np.zeros(T.shape) + rewards                    # (6,2,6)
+# Make reward = zero for the invalid transitions
 R[T == 0] = 0
 
-V = np.zeros(N_STATES)
 Q = np.zeros([N_STATES, N_ACTIONS])
-
-sample = R
 Q_k = Q.copy()
 Q = Q_k + np.sum(T * (R + gamma*np.max(Q, axis=1)), axis = 2)
-
 V = np.max(Q, axis=1)
-P = np.argmax(Q, axis=1) # Policy
+Policy = np.argmax(Q, axis=1)
+
+print(Q)
+print(V)
+print(Policy)
